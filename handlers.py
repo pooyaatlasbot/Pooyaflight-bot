c22c827554424084a8866f008a239726718472b2
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-05 02:52:44 +0330

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -2,7 +2,10 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from telegram import ReplyKeyboardMarkup
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
+    await update.message.reply_text(
+    "به آموزشگاه خلبانی پویا فلایت خوش آمدید.",
+    reply_markup=reply_markup
+)
 keyboard = [
     ["✈️ دوره‌های آموزشی"],
     ["💰 شهریه", "📝 ثبت‌نام"],
