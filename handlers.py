8be1260a728bf762014c32e8e45b6417e8ebbe22
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-05 02:48:25 +0330

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-
+from telegram import ReplyKeyboardMarkup
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "🛩 به ربات رسمی آموزشگاه خلبانی پویا فلایت خوش آمدید."
