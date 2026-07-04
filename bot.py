f5a0e363ef37ca40c599d5578b5ed69a20e995fc
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-05 02:40:52 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,25 +10,7 @@ from database import init_db
 
 
 
-    text = update.message.text
 
-    if text == "✈️ دوره‌های آموزشی":
-        await update.message.reply_text(
-            "✈️ آموزش خلبانی فوق سبک\n"
-            "👨‍✈️ آموزش تئوری\n"
-            "🛩️ آموزش عملی"
-        )
-
-    elif text == "🌐 وب‌سایت":
-        await update.message.reply_text("https://pooyaflight.ir")
-
-    elif text == "📞 تماس با ما":
-        await update.message.reply_text("02634490401")
-
-    elif text == "📍 آدرس":
-        await update.message.reply_text(
-            "استان البرز - فرودگاه آزادی - آموزشگاه خلبانی پویا فلایت"
-        )
 init_db()
 
 app = Application.builder().token(TOKEN).build()
