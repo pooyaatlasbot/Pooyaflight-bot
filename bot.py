582bc6c02caaeb14f1f4c6a0571bfec1585f8e1e
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-05 02:38:56 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,9 +37,3 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, menu))
 
 app.run_polling()
-app = Application.builder().token(TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, menu))
-
-app.run_polling()
