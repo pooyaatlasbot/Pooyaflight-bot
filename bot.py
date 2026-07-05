ab97cb39e4b1ca6348bc94b41d5aaf658563945b
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-05 04:41:27 +0330

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+
+from config import BOT_TOKEN
+from handlers import start, menu
+from database import init_db
+
+
+def main():
+    init_db()
+
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, menu)
+    )
+
+    print("✅ Pooya Atlas Bot Started...")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
